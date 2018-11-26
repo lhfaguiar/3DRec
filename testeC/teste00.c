d11349968715328c712dbe0414c78f5abbcfca8c
lhfaguiar <lais.harumi.aguiar@usp.br>
2018-11-25 23:12:29 -0200

    Update teste00.c

--- testeC/teste00.c
+++ testeC/teste00.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 
 float sigmoid(float x) {
      float exp_value;
@@ -16,7 +17,9 @@ float sigmoid(float x) {
 }
 
  int main() {
- 	clock_t begin = clock();
+ 	// clock_t begin = clock();
+ 	struct timeval  tv1, tv2;
+ 	gettimeofday(&tv1, NULL);
 
  	FILE *weights;
  	FILE *data;
@@ -31,16 +34,15 @@ float sigmoid(float x) {
  			float fst, scnd;
 	 		float mult = 0;
 			
-
 			float dataArray[10];
 			float weightsArray[10];
 
-		 	for (int i = 0; 9; i++) {
+		 	for (int i = 0; i < 10; i++) {
 		 		fscanf(data, "%1f", &dataArray[i]);
  				fscanf(weights, "%1f", &weightsArray[i]);
 		 	}
 
-		 	for (int j = 0; 9; j++) {
+		 	for (int j = 0; j < 10; j++) {
 		 		fst = dataArray[j];
 		 		scnd = weightsArray[j];
 
@@ -59,10 +61,15 @@ float sigmoid(float x) {
 	printf("a saída é: %f \n", sum);
 
 
- 	clock_t end = clock();
- 	double time_spent = (double)(end - begin);
+ 	// clock_t end = clock();
+ 	// double time_spent = (double)(end - begin);
 
- 	printf("Time spent: %f \n", time_spent);
+ 	gettimeofday(&tv2, NULL);
+ 	printf ("Total time = %f seconds\n",
+         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+         (double) (tv2.tv_sec - tv1.tv_sec));
+
+ 	// printf("Time spent: %f \n", time_spent);
 
  	return 0;
  }
